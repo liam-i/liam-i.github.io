2c2a450f5d0cc2ebebe803240addaf77d500a563
Liam <liam_i@163.com>
2024-10-18 18:45:04 +0800

    Deploying to gh-pages from @ 96bf468 🚀

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1729248303',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ 'https://liam-i.goatcounter.com/counter/' ] }, purge: false };
