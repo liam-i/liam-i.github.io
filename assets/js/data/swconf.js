8d1db1b7cd4ba41f12a67da984244d9817fe3d27
Liam <liam_i@163.com>
2025-11-20 00:04:33 +0400

    Deploying to gh-pages from @ 47246c1 🚀

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1763582672',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ 'https://liam-i.goatcounter.com/counter/' ] }, purge: false };
